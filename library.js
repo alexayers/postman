73bc4117b97a8139f4598a795e1350f74b80a48d
Alex Ayers <alexayers@users.noreply.github.com>
2020-05-29 13:11:44 -0400

    Update library.js

--- library.js
+++ library.js
@@ -1,6 +1,11 @@
-(class DateUtils {
- 
-    test() {
-        console.log("This is a test");
+
+// Create a new class and assign to variable
+var dateUtils = class DateUtils {
+    constructor(){}
+    getDateNow() {
+        return new Date().toISOString().slice(0,19);
     }
-})
+    getCurrentDay() {
+        return this.getDateNow().substr(0, this.getDateNow().length - 8) + '00:00:00';
+    }
+}
